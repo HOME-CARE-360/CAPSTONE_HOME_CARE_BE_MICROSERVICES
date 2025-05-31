76de682a3008abb041b520dd3824491053ee5616
tientran1234 <tranngoctien29112003@gmail.com>
2025-05-31 18:04:54 +0700

    push for build

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -3,8 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AuthReponsitory } from './auth.repo';
 import { GoogleService } from './google.service';
+import { CommonModule } from 'libs/common/src';
 
 @Module({
+  imports: [CommonModule],
   controllers: [AuthController,],
   providers: [AuthService, AuthReponsitory, GoogleService],
 })
--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -1,8 +1,54 @@
-import { Module } from '@nestjs/common';
-import { CommonService } from './common.service';
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './services/prisma.service';
+import { HashingService } from './services/hashing.service';
+import { TokenService } from './services/token.service';
+import { JwtModule } from '@nestjs/jwt';
+import { AIService } from './services/AI.services';
+import { AccessTokenGuard } from './guards/access-token.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthenticationGuard } from './guards/authentication.guard';
+import { PaymentAPIKeyGuard } from './guards/api-key.guard';
+import { EmailService } from './services/email.service';
+import { SharedUserRepository } from './repositories/shared-user.repo';
+import { TwoFactorService } from './services/2fa.service';
 
+import { SharedProviderRepository } from './repositories/share-provider.repo';
+import { SharedRoleRepository } from './repositories/shared-role.repo';
+import { S3Service } from './services/S3.service';
+import { ShareShiftTemplateRepository } from './repositories/shared-shift-template.repo';
+import { ShareStaffRepository } from './repositories/shared-staff.repo';
+import { SharedScheduleRepository } from './repositories/share-schedule.repo';
+import { SharedCategoryRepository } from './repositories/shared-category.repo';
+const sharedServices = [
+  PrismaService,
+  HashingService,
+  TokenService,
+  AIService,
+  PaymentAPIKeyGuard,
+  EmailService,
+  SharedUserRepository,
+  TwoFactorService,
+  SharedProviderRepository
+  , SharedRoleRepository,
+  S3Service,
+  ShareShiftTemplateRepository,
+  ShareStaffRepository,
+  SharedScheduleRepository,
+  SharedCategoryRepository
+
+]
+
+@Global()
 @Module({
-  providers: [CommonService],
-  exports: [CommonService],
+  providers: [
+    ...sharedServices,
+    AccessTokenGuard,
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticationGuard,
+    },
+  ],
+  exports: sharedServices,
+  imports: [JwtModule],
 })
-export class CommonModule {}
+export class CommonModule { }

f433b36b4b5254c459acbd32b931555d2c52b8b1
tientran1234 <tranngoctien29112003@gmail.com>
2025-05-31 16:53:49 +0700

    edit file config

--- apps/auth/src/config.ts
+++ apps/auth/src/config.ts
@@ -2,14 +2,14 @@ import z from 'zod'
 import fs from 'fs'
 import path from 'path'
 import { config } from 'dotenv'
+
 const envPath = path.resolve(__dirname, '../../../apps/auth/.env')
 
-if (!fs.existsSync(envPath)) {
-    console.log('Không tìm thấy file .env tại:', envPath)
-    process.exit(1)
+// Nếu là môi trường development mới cần load file .env
+if (process.env.NODE_ENV !== 'production' && fs.existsSync(envPath)) {
+    config({ path: envPath })
 }
 
-config({ path: envPath })
 const configSchema = z.object({
     DATABASE_URL_MAIN: z.string(),
     ACCESS_TOKEN_SECRET: z.string(),
@@ -28,11 +28,10 @@ const configSchema = z.object({
 const configServer = configSchema.safeParse(process.env)
 
 if (!configServer.success) {
-    console.log('Các giá trị khai báo trong file .env không hợp lệ')
+    console.error('Các giá trị khai báo trong biến môi trường không hợp lệ')
     console.error(configServer.error)
     process.exit(1)
 }
 
 const envConfig = configServer.data
-
 export default envConfig

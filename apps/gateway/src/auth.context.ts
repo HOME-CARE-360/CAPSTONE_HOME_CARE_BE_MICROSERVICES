87faa3ff810a967b6bd740a502369dce40f7d011
tientran1234 <tranngoctien29112003@gmail.com>
2025-06-02 20:58:33 +0700

    edit docker file

--- apps/gateway/src/auth.context.ts
+++ apps/gateway/src/auth.context.ts
@@ -1,71 +1,20 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AUTH_SERVICE, LoggerModule } from '@app/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
-import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { authContext } from './auth.context';
+import { UnauthorizedException } from '@nestjs/common';
+import { app } from './app';
+import { ClientProxy } from '@nestjs/microservices';
 
-@Module({
-    imports: [
-        GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
-            driver: ApolloGatewayDriver,
-            useFactory: (configService: ConfigService) => ({
-                server: {
-                    context: authContext,
-                },
-                gateway: {
-                    supergraphSdl: new IntrospectAndCompose({
-                        subgraphs: [
-                            {
-                                name: 'reservations',
-                                url: configService.getOrThrow('RESERVATIONS_GRAPHQL_URL'),
-                            },
-                            {
-                                name: 'auth',
-                                url: configService.getOrThrow('AUTH_GRAPHQL_URL'),
-                            },
-                            {
-                                name: 'payments',
-                                url: configService.getOrThrow('PAYMENTS_GRAPHQL_URL'),
-                            },
-                        ],
-                    }),
-                    buildService({ url }) {
-                        return new RemoteGraphQLDataSource({
-                            url,
-                            willSendRequest({ request, context }) {
-                                request.http.headers.set(
-                                    'user',
-                                    context.user ? JSON.stringify(context.user) : null,
-                                );
-                            },
-                        });
-                    },
-                },
+import { lastValueFrom } from 'rxjs';
+import { AUTH_SERVICE } from 'libs/common/src/constants/service-name.constant';
+
+export const authContext = async ({ req }) => {
+    try {
+        const authClient = app.get<ClientProxy>(AUTH_SERVICE);
+        const user = await lastValueFrom(
+            authClient.send('authenticate', {
+                Authentication: req.headers?.authentication,
             }),
-            inject: [ConfigService],
-        }),
-        ClientsModule.registerAsync([
-            {
-                name: AUTH_SERVICE,
-                useFactory: (configService: ConfigService) => ({
-                    transport: Transport.TCP,
-                    options: {
-                        host: configService.getOrThrow('AUTH_HOST'),
-                        port: configService.getOrThrow('AUTH_PORT'),
-                    },
-                }),
-                inject: [ConfigService],
-            },
-        ]),
-        ConfigModule.forRoot({
-            isGlobal: true,
-        }),
-        LoggerModule,
-    ],
-    controllers: [],
-    providers: [],
-})
-export class GatewayModule { }
+        );
+        return { user };
+    } catch (err) {
+        throw new UnauthorizedException(err);
+    }
+};

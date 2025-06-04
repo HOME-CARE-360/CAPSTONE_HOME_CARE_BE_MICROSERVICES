69fd77afc5221a72e62dc46c8469ecbcabf5e6ba
tientran1234 <tranngoctien29112003@gmail.com>
2025-06-04 22:40:49 +0700

    logs error

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -5,11 +5,17 @@ import { AuthReponsitory } from './auth.repo';
 import { GoogleService } from './google.service';
 import { CommonModule } from 'libs/common/src';
 import { ConfigModule } from 'libs/common/src/modules/config.module';
+import { APP_PIPE } from '@nestjs/core';
+import CustomZodValidationPipe from 'libs/common/src/pipes/custom-zod-validation.pipe';
 
 @Module({
-  imports: [CommonModule, ConfigModule
+  imports: [CommonModule, ConfigModule,
+
   ],
   controllers: [AuthController,],
-  providers: [AuthService, AuthReponsitory, GoogleService],
+  providers: [AuthService, AuthReponsitory, GoogleService, {
+    provide: APP_PIPE,
+    useClass: CustomZodValidationPipe
+  }],
 })
 export class AuthModule { }
